f0d173971b9433f40c85fda94de4961cb2c5f008
giordanoale <a.giordano89@gmail.com>
2021-03-11 16:55:57 +0100

    Aggiunta una seconda soluzione

--- main.js
+++ main.js
@@ -2,13 +2,31 @@ var btn = $("a > i");
 var hamburger = $(".hamburger-menu");
 var visible = false;
 
+
+//Prima soluzione
+
+// btn.click(
+//     function() {
+//         if(visible == false){
+//             hamburger.show("500");
+//             visible = true;
+//         } else {
+//             hamburger.hide("500");
+//             visible = false;
+//         }
+//     }
+// );
+
+
+//Seconda Soluzione
+
 btn.click(
     function() {
         if(visible == false){
-            hamburger.show();
+            hamburger.addClass("active");
             visible = true;
         } else {
-            hamburger.hide();
+            hamburger.removeClass("active");
             visible = false;
         }
     }
