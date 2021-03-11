65177f877699fd20a601a8130ef25c1f8127fe9e
giordanoale <a.giordano89@gmail.com>
2021-03-11 17:07:54 +0100

    Fix

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 //Prima soluzione
 
-// var btn = $("a > i");
-// var hamburger = $(".hamburger-menu");
-// var visible = false;
+var btn = $("a > i");
+var hamburger = $(".hamburger-menu");
+var visible = false;
 
-// btn.click(
-//     function() {
-//         if(visible == false){
-//             hamburger.show("500");
-//             visible = true;
-//         } else {
-//             hamburger.hide("500");
-//             visible = false;
-//         }
-//     }
-// );
+btn.click(
+    function() {
+        if(visible == false){
+            hamburger.show("500");
+            visible = true;
+        } else {
+            hamburger.hide("500");
+            visible = false;
+        }
+    }
+);
 
 
 //Seconda Soluzione
 
-var btn = $("a > i");
-var hamburger = $(".hamburger-menu");
+// var btn = $("a > i");
+// var hamburger = $(".hamburger-menu");
 
-btn.click(
-    function() {
-       hamburger.toggleClass("active");  
-    }
-);
+// btn.click(
+//     function() {
+//        hamburger.toggleClass("active");  
+//     }
+// );
